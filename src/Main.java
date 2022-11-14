b0cc21dcec5f2fa1fbcddedf4dcc2a2299b0da43
arfanxn <arfan2173@gmail.com>
2022-11-14 10:10:27 +0700

    soal 3 recommit

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,57 @@
 import javax.sound.midi.Soundbank;
 import java.math.BigInteger;
 import java.util.Scanner;
+import static java.lang.Character.*;
 
 public class Main {
     private static final Scanner scannerInput = new Scanner(System.in);
 
     public static void main(String[] args) {
-        float[] floatNumbers = new float[] {2.34f, 4.56f, 67.89f};
-        roundUp(floatNumbers);
+//        String[] employees = {"C01XX" , "J02XX" , "N03XX" , "P04XX"};
+//        countBusPassengersOnEachLevel(employees);
     }
+
+    private static void countBusPassengersOnEachLevel (String[] passengers) {
+        String[] busInfo = {"01-TopLevel", "02-MiddleLevel", "03-RegularStaff", "04-Staff"};
+        int topLevel = 0 ;
+        int middleLevel = 0 ;
+        int regularStaffLevel = 0 ;
+        int staffLevel = 0 ;
+
+        for(int i = 0; i < passengers.length ; i++) {
+           String passenger = passengers[i] ;
+           char firstChar = passenger.charAt(0);
+           char firstCharOfNIK = passenger.charAt(2);
+
+           if (
+                   (Character.compare(firstChar ,'C') == 0)
+                        && (Character.compare(firstCharOfNIK ,'1') == 0)
+           ) {
+                topLevel += 1;
+           }
+           else if(
+                   (Character.compare(firstChar ,'J') == 0)
+                   && (Character.compare(firstCharOfNIK ,'2') == 0)
+           ){
+               middleLevel += 1;
+           } else if(
+                   (Character.compare(firstChar ,'N') == 0)
+                           && (Character.compare(firstCharOfNIK ,'3') == 0)
+           ){
+               regularStaffLevel += 1;
+           } else {
+               staffLevel += 1;
+           }
+
+
+           System.out.println("Total TopLevel : " + topLevel);
+           System.out.println("Total MiddleLevel : " + middleLevel);
+           System.out.println("Total RegularStaffLevel : " + regularStaffLevel);
+           System.out.println("Total StaffLevel : " + staffLevel);
+
+        }
+    }
+
 
     private static void roundUp (float[] floats) {
         int[] roundedUps = new int[floats.length];
@@ -24,7 +67,4 @@ public class Main {
         }
     }
 
-    private static void () {
-
-    }
 }
